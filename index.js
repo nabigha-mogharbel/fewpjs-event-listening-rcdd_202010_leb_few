31365f1e92205dccdaea80eca44a946854d61ef9
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-15 21:50:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 function addingEventListener(){
 const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
 });}
+=======
+function 
+const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+>>>>>>> 4fc5cf85f9aa786ee72b2d4b7c4b44dc822dbf39
